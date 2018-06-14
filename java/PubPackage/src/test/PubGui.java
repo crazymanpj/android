bc29b8a700224e4fef71f7c997241bedaec34868
crazymanpj <pang456jian@163.com>
2018-06-14 08:21:58 +0800

    update
    update

--- java/PubPackage/src/test/PubGui.java
+++ java/PubPackage/src/test/PubGui.java
@@ -5,7 +5,9 @@ import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.io.*;
+import java.util.regex.*;
 
 
 
@@ -26,26 +28,75 @@ public class PubGui {
 		frame.setVisible(true);
 	}
 	
-	private static void updateText(String dirPath, String versioncode, boolean isupdateText) {
+	private static void updateConfig(String dirPath, String versioncode, boolean isupdateText) {
+		System.out.println("updateConfig");
+		System.out.println(isupdateText);
 		String gobalConfigFile = "gobal_config.py";
         FileOutputStream out = null;
         FileOutputStream outSTr = null;
         BufferedOutputStream Buff = null;
         FileWriter fw = null;
         FileInputStream in = null;
+        InputStreamReader isr = null;
         
         try{
-        	out = new FileOutputStream(new File("d:\\kuaipan\\python\\autopublishpackage\\script\\gobal_config.py"));
+        	in = new FileInputStream(new File("C:\\Users\\PJ\\Desktop\\gobal_config.py"));
+        	isr = new InputStreamReader(in);
+        	int ch = 0;
+        	String text = "";
+        	while((ch = isr.read()) != -1) {
+//        		System.out.print((char) ch);
+        		text = text + (char) ch;
+        	}
+        	
+//        	System.out.println(text);
+        	String pattern = "(DIR = r')(\\S+)(')";
+        	String pattern2 = "(APKVER = ')(\\S+)(')";
+        	String pattern3 = "(IS_UPDATE_TEXT = )(\\S)";
+        	
+        	Pattern r = Pattern.compile(pattern);
+        	Matcher m = r.matcher(text);
+            if (m.find( )) {
+                text = text.replace(m.group(2), dirPath);
+            } else {
+               System.out.println("NO MATCH");
+            }
+           
+            
+        	Pattern r2 = Pattern.compile(pattern2);
+        	Matcher m2 = r2.matcher(text);
+            if (m2.find()) {
+                text = text.replace(m2.group(2), versioncode);
+            } else {
+               System.out.println("NO MATCH");
+            }
+          
+        	Pattern r3 = Pattern.compile(pattern3);
+        	Matcher m3 = r.matcher(text);
+            if (m3.find()) {
+            	if(isupdateText == true) {
+            		System.out.println(m3.group(2));
+            		text = text.replace(m3.group(2), "1");
+            	}
+            	else {
+                    text = text.replace(m3.group(2), "0");
+            	}
+  
+            } else {
+               System.out.println("NO MATCH");
+            }
+
+            System.out.println(text);
      
         }
         catch(Exception e) {
         	e.printStackTrace();
         } finally {
             try {
-                fw.close();
-                Buff.close();
-                outSTr.close();
-                out.close();
+//                fw.close();
+//                Buff.close();
+//                outSTr.close();
+//                out.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -123,7 +174,12 @@ public class PubGui {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("点击按钮");
 				System.out.println(dirText.getText());
+				String dirPath = dirText.getText();
+				String versionCode = versioncodeText.getText();
+				boolean isUpdateText = isupdateText.isSelected();
 				System.out.println(isupdateText.isSelected());
+				updateConfig(dirPath, versionCode, isUpdateText);
+				
 				if(cb1.isSelected()) {
 					String cmd = new String("python yyb.py");
 					try {
